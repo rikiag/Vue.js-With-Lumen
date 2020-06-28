db87de33c28ea187244bd833fc8f0f5e7b50cf82
RIki Agusnaidi <r.agusnaidi@gmail.com>
2020-06-28 23:26:01 +0700

    Mengaktifkan CORS di Lumen

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -72,9 +72,9 @@ $app->configure('app');
 |
 */
 
-// $app->middleware([
-//     App\Http\Middleware\ExampleMiddleware::class
-// ]);
+$app->middleware([
+    App\Http\Middleware\CorsMiddleware::class
+]);
 
 // $app->routeMiddleware([
 //     'auth' => App\Http\Middleware\Authenticate::class,
@@ -91,6 +91,7 @@ $app->configure('app');
 |
 */
 
+$app->register(App\Providers\CatchAllOptionsRequestsProvider::class);
 // $app->register(App\Providers\AppServiceProvider::class);
 // $app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
